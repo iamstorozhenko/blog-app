b7bd9852e66e5ab67d5bee65cb55411905af09fd
iamstorozhenko <iamantonstorozhenko@gmail.com>
2023-04-16 19:56:50 +0300

    created login page

---
+++ client/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { Roboto } from "next/font/google";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <main>hello world</main>;
+}
