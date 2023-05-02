091ce4c5450fcb4fd28ca6e7fe5d63a316acc7f8
iamstorozhenko <iamantonstorozhenko@gmail.com>
2023-05-02 19:18:57 +0300

    updated protected middleware

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -12,21 +12,16 @@ const protect = (
   next: NextFunction
 ): void => {
   const authHeader = req.headers.authorization;
-
   if (!authHeader) {
     res.status(401).json({ error: "Missing jwt token" });
     return;
   }
-
   const token = authHeader.split(" ")[1];
-
   if (!token) {
     res.status(401).json({ error: "Missing token" });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN as Secret);
-
     req.user = decoded as IUser;
     next();
   } catch (error) {
