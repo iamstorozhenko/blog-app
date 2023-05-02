2373c7a459bfa9f987d93875f425dad2b0b1f5e5
iamstorozhenko <iamantonstorozhenko@gmail.com>
2023-05-02 19:19:45 +0300

    updated user router / deleted unused route

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createUser, loginUser, getMe } from "../controller/userController";
+import { createUser, loginUser } from "../controller/userController";
 import protect from "../middleware/authMiddleware";
 
 const router: Router = Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
-router.get("/me", protect, getMe);
 
 export default router;
